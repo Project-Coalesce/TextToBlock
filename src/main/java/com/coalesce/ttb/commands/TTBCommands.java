653c877ce537fc8a52ff41c2a3b47b6ee2b8330f
NJDaeger <noahdaeger@hotmail.com>
2017-04-09 01:48:45 -0400

    Testing discord

--- src/main/java/com/coalesce/ttb/commands/TTBCommands.java
+++ src/main/java/com/coalesce/ttb/commands/TTBCommands.java
@@ -1,10 +1,14 @@
 package com.coalesce.ttb.commands;
 
+import org.bukkit.command.CommandSender;
+
 /**
  * Created by NJDaeger on 4/9/2017.
  */
 public class TTBCommands {
     
-    
+    public void command(CommandSender sender, String alias, String[] args) {
+        return;
+    }
     
 }
