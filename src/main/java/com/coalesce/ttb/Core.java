73c260bb2c37f609397079c4dc23d7f49b5d9e47
NJDaeger <noahdaeger@hotmail.com>
2017-04-09 00:24:19 -0400

    Added inherited methods

--- src/main/java/com/coalesce/ttb/Core.java
+++ src/main/java/com/coalesce/ttb/Core.java
@@ -1,7 +1,20 @@
 package com.coalesce.ttb;
 
+import com.coalesce.plugin.CoPlugin;
+
 /**
  * Created by NJDaeger on 4/9/2017.
  */
-public class Core {
+public class Core extends CoPlugin {
+    
+    
+    @Override
+    public void onPluginEnable() throws Exception {
+        
+    }
+    
+    @Override
+    public void onPluginDisable() throws Exception {
+        
+    }
 }
