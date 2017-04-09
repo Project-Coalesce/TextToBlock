370ceb9dd582ff198ba0e2427dcf6fd95f3e7b9d
ElDoggo <spookyFalco@gmail.com>
2017-04-09 00:04:49 -0500

    Rename of main class commit

--- src/main/java/com/coalesce/ttb/Core.java
+++ src/main/java/com/coalesce/ttb/Core.java
@@ -2,12 +2,8 @@ package com.coalesce.ttb;
 
 import com.coalesce.plugin.CoPlugin;
 
-/**
- * Created by NJDaeger on 4/9/2017.
- */
 public class Core extends CoPlugin {
-    
-    
+
     @Override
     public void onPluginEnable() throws Exception {
         
