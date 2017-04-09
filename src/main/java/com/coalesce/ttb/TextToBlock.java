c591d4bacb010fad4506fd5ea6ec8b8fc3e823d0
NJDaeger <noahdaeger@hotmail.com>
2017-04-09 01:54:06 -0400

    This is what a command looks like

--- src/main/java/com/coalesce/ttb/TextToBlock.java
+++ src/main/java/com/coalesce/ttb/TextToBlock.java
@@ -1,12 +1,14 @@
 package com.coalesce.ttb;
 
+import com.coalesce.command.CommandLoader;
 import com.coalesce.plugin.CoPlugin;
+import com.coalesce.ttb.commands.TTBCommands;
 
 public class TextToBlock extends CoPlugin {
     
     @Override
     public void onPluginEnable() throws Exception {
-        
+        CommandLoader.addCommand(TTBCommands.class);
     }
     
     @Override
--- src/main/java/com/coalesce/ttb/commands/TTBCommands.java
+++ src/main/java/com/coalesce/ttb/commands/TTBCommands.java
@@ -1,5 +1,7 @@
 package com.coalesce.ttb.commands;
 
+import com.coalesce.command.registry.Cmd;
+import com.coalesce.command.registry.Executor;
 import org.bukkit.command.CommandSender;
 
 /**
@@ -7,6 +9,13 @@ import org.bukkit.command.CommandSender;
  */
 public class TTBCommands {
     
+    @Cmd(
+           name = "text",
+           desc = "This will be the desc",
+           usage = "/text <message> -b(block) -s(size) -f(font) -t(thickness)",
+           executor = Executor.PLAYER,
+           min = 1
+    )
     public void command(CommandSender sender, String alias, String[] args) {
         return;
     }
