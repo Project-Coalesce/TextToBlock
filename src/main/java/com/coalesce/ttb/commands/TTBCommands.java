4d386041b4a56cba1675be36c7931cc659f5abcb
NJDaeger <noahdaeger@hotmail.com>
2017-04-09 01:46:37 -0400

    Starting on Command class

---
+++ src/main/java/com/coalesce/ttb/commands/TTBCommands.java
@@ -0,0 +1,10 @@
+package com.coalesce.ttb.commands;
+
+/**
+ * Created by NJDaeger on 4/9/2017.
+ */
+public class TTBCommands {
+    
+    
+    
+}
--- src/main/java/com/coalesce/ttb/data/FontLoader.java
+++ src/main/java/com/coalesce/ttb/data/FontLoader.java
@@ -3,6 +3,5 @@ package com.coalesce.ttb.data;
 /**
  * Created by NJDaeger on 4/9/2017.
  */
-public
-class FontLoader {
+public class FontLoader {
 }
