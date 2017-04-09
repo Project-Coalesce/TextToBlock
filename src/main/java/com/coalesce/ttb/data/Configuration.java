cda5e60b0b2f7802c0f35c933ff6781007b300a8
NJDaeger <noahdaeger@hotmail.com>
2017-04-09 16:52:06 -0400

    Removed config

--- src/main/java/com/coalesce/ttb/data/Configuration.java
+++ src/main/java/com/coalesce/ttb/data/Configuration.java
@@ -1,20 +1,25 @@
 package com.coalesce.ttb.data;
 
+import com.coalesce.plugin.CoPlugin;
+import com.moandjiezana.toml.Toml;
+
 import java.io.File;
 
 /**
  * Created by NJDaeger on 4/9/2017.
  */
 public class Configuration {
-
-	private File fontfolder, path;
-
-	public Configuration() {
-		this.fontfolder = new File("plugins" + File.separator + "TextToBlock" + File.separator + "Fonts");
-	}
-
-	public File[] getFonts() {
-		return fontfolder.listFiles();
-	}
-
+    
+    private File fontfolder, path;
+    
+    public Configuration(CoPlugin plugin) {
+        Toml toml = new Toml().
+        this.fontfolder = new File("plugins" + File.separator + "TextToBlock" + File.separator + "Fonts");
+        
+    }
+    
+    public File[] getFonts() {
+        return fontfolder.listFiles();
+    }
+    
 }
