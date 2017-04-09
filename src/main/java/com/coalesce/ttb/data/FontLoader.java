a02259a3e60d6dc20a537aefdd9c9dac9420b393
ElDoggo <spookyFalco@gmail.com>
2017-04-09 13:16:00 -0500

    Made FontLoader extend CoModule

--- src/main/java/com/coalesce/ttb/data/FontLoader.java
+++ src/main/java/com/coalesce/ttb/data/FontLoader.java
@@ -1,7 +1,28 @@
 package com.coalesce.ttb.data;
 
-/**
- * Created by NJDaeger on 4/9/2017.
- */
-public class FontLoader {
+import com.coalesce.plugin.CoModule;
+import com.coalesce.plugin.CoPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public class FontLoader extends CoModule {
+
+    /**
+     * Create a new module
+     *
+     * @param plugin The plugin that's creating this module
+     * @param name   The name of this module
+     */
+    public FontLoader(@NotNull CoPlugin plugin, @NotNull String name) {
+        super(plugin, name);
+    }
+
+    @Override
+    protected void onEnable() throws Exception {
+
+    }
+
+    @Override
+    protected void onDisable() throws Exception {
+        //Do anything we need to do to save font data
+    }
 }
