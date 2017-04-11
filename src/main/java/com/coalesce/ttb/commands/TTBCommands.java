9bd4537a58d89bfc84c0ecc29031a7378fbbdb87
Sxtanna <ranald.taylor@gmail.com>
2017-04-11 00:13:39 -0400

    This is how you register them

--- src/main/java/com/coalesce/ttb/commands/TTBCommands.java
+++ src/main/java/com/coalesce/ttb/commands/TTBCommands.java
@@ -1,31 +1,41 @@
 package com.coalesce.ttb.commands;
 
+import com.coalesce.command.CommandModule;
 import com.coalesce.command.base.CmdContext;
 import com.coalesce.command.base.Command;
-import com.coalesce.ttb.TextToBlock;
+import com.coalesce.plugin.CoModule;
+import com.coalesce.plugin.CoPlugin;
 import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
 
-import static com.coalesce.command.base.Command.of;
+public final class TTBCommands extends CoModule {
 
-public final class TTBCommands {
-	
-    public TTBCommands() {
-        Command textCommand = of(this::text, "text").onlyPlayer();
-    }
-    
-    /* command would ideally look like this: /text <{message}> [-f, -s, -i, -b, -u]
-    -f = font
-    -s = size
-    -i = italics
-    -b = bold
-    -u = underline
-     */
+
+	public TTBCommands(@NotNull CoPlugin plugin, CommandModule commands) {
+		super(plugin, "TTB Commands");
+
+		commands.add(Command.of(this::text, "Text").onlyPlayer());
+	}
+
+	@Override
+	protected void onEnable() throws Exception {
+	}
+
+	@Override
+	protected void onDisable() throws Exception {
+
+	}
+
+	//The command would ideally look like this: /text <{message}> [-f, -s, -i, -b, -u]
     private void text(CmdContext context) {
-        if (context.noArgs(ChatColor.RED + "Please provide command arguments.")) return;
-    }
+		if (context.noArgs(ChatColor.RED + "Please input the text")) return;
+
+		String text = context.joinArgs();
+		// TODO: 4/10/2017 Create gui with this text
+	}
     
     private void undo(CmdContext context) {
-    	
+	    
     }
     
     private void redo(CmdContext context) {
